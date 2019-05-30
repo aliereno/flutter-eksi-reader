369da11821b862a8c31ee5b0699f68a57f188110
reivnn <36535188+reivnn@users.noreply.github.com>
2019-05-31 00:08:15 +0300

    added screenshot

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Flutter application for reading https://eksisozluk.com.
 
-
+![Screenshot](images/Screenshot_1.png)
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ekşi with Flutter',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
