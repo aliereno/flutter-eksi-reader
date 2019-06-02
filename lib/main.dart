6cf22b90df6f5073bd01084849775dc50a59ee51
reivnn <36535188+reivnn@users.noreply.github.com>
2019-06-02 16:50:03 +0300

    added nicest posts option

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:html/parser.dart';
 import 'package:html/dom.dart' as dom; 
 import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_html_view/flutter_html_view.dart';
+import 'package:url_launcher/url_launcher.dart'; 
 
 
 void main() => runApp(MyApp());
@@ -67,13 +68,11 @@ class HomePageState extends State<HomePage>{
     setState((){
       listTopics = localListTopics;
     });
-    return "Success";
   }
 
   Future<Null> refreshList() async {
     setState((){
       listTopics = [];
-      print("*********REFRESHED*********");
       getTopics();
     });
 
@@ -112,25 +111,39 @@ class HomePageState extends State<HomePage>{
           );
   }
 }
+class TopicDetail extends StatefulWidget {
+  Topic topic;
+  TopicDetail(this.topic);
 
-class TopicDetail extends StatelessWidget {
+  @override
+  TopicDetailState createState() => new TopicDetailState();
+}
+
+class TopicDetailState extends State<TopicDetail> {
   Topic topicObject;
-  TopicDetail(this.topicObject);
   List topics;
   List posts;
   int pages;
+  String url;
+  String baseUrl;
+
   List<Post> listPosts = [];
-  String url; 
+  @override
+    void initState(){
+      super.initState();
+      topicObject = widget.topic;
+      url = "https://eksisozluk.com" + topicObject.url; //.replaceAll("?a=popular", "")
+      baseUrl = url.replaceAll("?a=popular", "");
+    }
 
   Future getPosts() async {
-    url = "https://eksisozluk.com" + topicObject.url;
     var client = Client();
     Response response = await client.get(url);
     var document = parse(response.body);
     var postset = document.querySelectorAll('#entry-item-list > li');
     
     for (var post in postset) { 
-      var vContent = post.querySelector('div.content').text;
+      var vContent = post.querySelector('div.content').outerHtml;
       var vAuthor = post.attributes['data-author']; 
       var vFavCount = post.attributes['data-favorite-count']; 
       var vDate = post.querySelector('footer > div.info > a.entry-date.permalink').text; 
@@ -142,9 +155,27 @@ class TopicDetail extends StatelessWidget {
 
       listPosts.add(postObject);
     }
-
     return listPosts;
   }
+  Future<Null> getNicePost() async {
+    setState((){
+      url = baseUrl + "?a=nice";
+      listPosts = [];
+      getPosts();
+    });
+
+    return null;
+  } 
+  Future<Null> getDailyNicePost() async {
+    setState((){
+      url = baseUrl + "?a=dailynice";
+      listPosts = [];
+      getPosts();
+    });
+
+    return null;
+  } 
+
   @override
   Widget build (BuildContext ctxt) {
     return new Scaffold(
@@ -152,31 +183,60 @@ class TopicDetail extends StatelessWidget {
         centerTitle: true,
         title: FittedBox(fit:BoxFit.fitWidth, 
         child: Text(topicObject.title)
-        )
+        ),
+          bottom: PreferredSize(
+            preferredSize: Size.square(10.0),
+            child:  Row(
+              children: <Widget>[
+                Spacer(),
+                Text("şükela: "),
+                Spacer(flex: 1),
+                GestureDetector(
+                  onTap: (){getNicePost();},
+                  child: Text("tümü",),
+                ),
+                Spacer(flex: 1),
+                GestureDetector(
+                  onTap: (){getDailyNicePost();},
+                  child: Text("bugün",),
+                ),
+                Spacer(flex: 15),
+              ]
+            ),
+          ),
       ),
       body: Container(
-          child: FutureBuilder(
-            future: getPosts(),
-            builder: (BuildContext context, AsyncSnapshot snapshot){
-              if(snapshot.data == null){
-                return Container(
-                  child: Center(
-                    child: Text("Loading...")
-                  )
-                );
-              } else {
-                return ListView.builder(
+        child: FutureBuilder(
+          future: getPosts(),
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            if(snapshot.data == null){
+              return Container(
+                child: Center(
+                  child: Text("Loading...")
+                )
+              );
+            } else {
+              return ListView.builder(
                   itemCount: snapshot.data.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return ListTile(   
-                      title: Text(snapshot.data[index].content.toString()),
-                      subtitle: Text(snapshot.data[index].favoriteCount+" fav | "+snapshot.data[index].author +" | " +snapshot.data[index].date, textAlign: TextAlign.right,),
+                    return Container(
+                      padding: const EdgeInsets.only(top: (15.0)),
+                      child: ListTile(   
+                        title: Html(
+                          data: snapshot.data[index].content.toString(),
+                          onLinkTap: (var u){
+                            _launchURL(u);
+                          },
+                          ),
+                        subtitle: Text(snapshot.data[index].favoriteCount+" fav | "+ snapshot.data[index].date+" | " +snapshot.data[index].author, textAlign: TextAlign.right,),
+                      ),
                     );
-                  },
+                  }
                 );
+                  
               }
-            },
-          ),
+          },
+        ),
         ),
     );
   }
@@ -194,4 +254,12 @@ class Topic {
     String url;
     int comments;
     bool self;
+}
+_launchURL(var u) async {
+  String url = u;
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
 }
